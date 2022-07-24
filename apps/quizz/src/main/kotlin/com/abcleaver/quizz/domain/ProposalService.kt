4a9f1201013077919b9324a3d05bd5f88a81055c
Swan BENOIT <swan.benoit@gmail.com>
2022-07-24 11:53:33 +0200

    refactor:add current letter to proposal

--- apps/quizz/src/main/kotlin/com/abcleaver/quizz/domain/ProposalService.kt
+++ apps/quizz/src/main/kotlin/com/abcleaver/quizz/domain/ProposalService.kt
@@ -4,9 +4,12 @@ import com.abcleaver.quizz.port.ImageOut
 
 class ProposalService constructor(private val imageOut: ImageOut) {
   fun generate(letter: Letter): Proposal {
-    val content = (1..4)
+    val content = (1..3)
       .map { AlphabetService.getRandomLetter(letter) }
-      .associateWith { imageOut.getImage(it) }
+      .map { it to imageOut.getImage(it) }
+      .plus(Pair(letter, imageOut.getImage(letter)))
+      .shuffled()
+      .toMap()
 
     return Proposal(content)
   }
